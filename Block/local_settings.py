4525eb9ca2fd1d0ee85ce454db8d314b48bea91d
ravshan_yusupov <yusupovravshan001@gmail.com>
2022-03-28 21:24:11 +0500

    fuck of

--- Block/local_settings.py
+++ Block/local_settings.py
@@ -1,6 +1,6 @@
 DEBUG = True
 ALLOWED_HOSTS = ['*']
-# HOST = 'https://9cc3-213-230-82-97.ngrok.io'
-HOST = 'https://z-z-zbot.herokuapp.com/'
+HOST = 'https://9cc3-213-230-82-97.ngrok.io'
+# HOST = 'https://z-z-zbot.herokuapp.com/'
 
 
